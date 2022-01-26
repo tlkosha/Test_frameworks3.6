4b50f74ab0c41bf53d9dd8b5361ab85a5b8c4393
tlkosha <95431054+tlkosha@users.noreply.github.com>
2022-01-26 16:36:35 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,7 @@
+import time
 
+def test_add_basket(browser):
+	link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+	browser.get(link)
+	time.sleep(5)
+	assert browser.find_element_by_css_selector('.btn-add-to-basket')
